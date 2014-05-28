ad7b1b8ef358405b08e0cf9f0dfa2b23849c707d
Alexey Vasiliev <leopard.not.a@gmail.com>
2014-05-28 16:20:14 +0300

    codes

--- source/codes/snippets/speed_like.sql
+++ source/codes/snippets/speed_like.sql
@@ -3,7 +3,7 @@ prefix_test(#  tag    text primary key,
 prefix_test(#  name      text not null,
 prefix_test(#  shortname text,
 prefix_test(#  status    char default 'S',
-prefix_test(# 
+prefix_test(#
 prefix_test(#  check( status in ('S', 'R') )
 prefix_test(# );
 NOTICE:  CREATE TABLE / PRIMARY KEY will create implicit index "tags_pkey" for table "tags"
@@ -16,7 +16,7 @@ prefix_test(#  tag    text primary key,
 prefix_test(#  name      text not null,
 prefix_test(#  shortname text,
 prefix_test(#  status    char default 'S',
-prefix_test(# 
+prefix_test(#
 prefix_test(#  check( status in ('S', 'R') )
 prefix_test(# );
 NOTICE:  CREATE TABLE / PRIMARY KEY will create implicit index "invalid_tags_pkey" for table "invalid_tags"
@@ -25,19 +25,19 @@ CREATE TABLE
 
 
 prefix_test=# select count(*) from tags;
- count 
+ count
 -------
  11966
 (1 row)
 
 prefix_test=# select count(*) from invalid_tags;
- count 
+ count
 -------
  11966
 (1 row)
 
 # EXPLAIN ANALYZE select * from invalid_tags where lower(tag) LIKE lower('0146%');
-                                                 QUERY PLAN                                                 
+                                                 QUERY PLAN
 ------------------------------------------------------------------------------------------------------------
  Seq Scan on invalid_tags  (cost=0.00..265.49 rows=60 width=26) (actual time=0.359..20.695 rows=1 loops=1)
    Filter: (lower(tag) ~~ '0146%'::text)
@@ -45,7 +45,7 @@ prefix_test=# select count(*) from invalid_tags;
 (3 rows)
 
 # EXPLAIN ANALYZE select * from invalid_tags where lower(tag) LIKE lower('0146%');
-                                                 QUERY PLAN                                                 
+                                                 QUERY PLAN
 ------------------------------------------------------------------------------------------------------------
  Seq Scan on invalid_tags  (cost=0.00..265.49 rows=60 width=26) (actual time=0.549..19.503 rows=1 loops=1)
    Filter: (lower(tag) ~~ '0146%'::text)
@@ -53,7 +53,7 @@ prefix_test=# select count(*) from invalid_tags;
 (3 rows)
 
 # EXPLAIN ANALYZE select * from tags where lower(tag) LIKE lower('0146%');
-                                                      QUERY PLAN                                                       
+                                                      QUERY PLAN
 -----------------------------------------------------------------------------------------------------------------------
  Bitmap Heap Scan on tags  (cost=5.49..97.75 rows=121 width=26) (actual time=0.054..0.057 rows=1 loops=1)
    Filter: (lower(tag) ~~ '0146%'::text)
@@ -63,7 +63,7 @@ prefix_test=# select count(*) from invalid_tags;
 (5 rows)
 
 # EXPLAIN ANALYZE select * from tags where lower(tag) LIKE lower('0146%');
-                                                      QUERY PLAN                                                       
+                                                      QUERY PLAN
 -----------------------------------------------------------------------------------------------------------------------
  Bitmap Heap Scan on tags  (cost=5.49..97.75 rows=121 width=26) (actual time=0.025..0.025 rows=1 loops=1)
    Filter: (lower(tag) ~~ '0146%'::text)
