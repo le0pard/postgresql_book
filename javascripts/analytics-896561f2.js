bd9c055497a232d847be5a1c2e7b692c12f7aecf
le0pard <le0pard@users.noreply.github.com>
2020-10-19 21:12:14 +0000

    Deploying to gh-pages from  @ b8c9f41de0ed9533189cfaaabb7e9bca3e8e610f 🚀

--- javascripts/analytics-896561f2.js
+++
@@ -1,1 +0,0 @@
-(function(){$(function(){return $("#downloadLink").click(function(){return function(){return ga("send","event","Download","PDF","Download book version 5")}}()),$("#readLink").click(function(){return function(){return ga("send","event","Read","HTML","Open html book version 5")}}())})}).call(this);
